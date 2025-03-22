706c7d7b2bad2880024a3e51a88b8db6e98ef651
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-03-22 23:49:06 +0900

    ActiveStrageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:title, :body, :learning_date,
+        params.require(:post).permit(:title, :body, :learning_date, :image, 
         daily_question_attributes: [ :body, :question_answer ])
       end
 end
