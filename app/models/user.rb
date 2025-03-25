def401f1fd4394b60a1ceb13e836eb7bd17543f5
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-03-25 15:36:43 +0900

    Usernameのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 10 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
