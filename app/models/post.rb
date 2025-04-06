0bffbe83245158d086a922d6ad86e85be038ae9e
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-04-06 09:10:25 +0900

    モデルの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,18 @@ class Post < ApplicationRecord
     belongs_to :user
     has_one :daily_question, dependent: :destroy
     has_one_attached :image
-    accepts_nested_attributes_for :daily_question
+    has_many :likes, dependent: :destroy
+    has_many :liked_users, through: :likes, source: :user
 
+    accepts_nested_attributes_for :daily_question
+    
     validates :title, presence: true, length: { maximum: 20 }
     validates :body, presence: true, length: { maximum: 400 }
     validates :learning_date, presence: true
+
+    def liked_by?(user)
+      likes.exists?(user_id: user.id)
+    end
 
     private
 
