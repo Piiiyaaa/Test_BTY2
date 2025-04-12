6018300373fb354ed1d33d33087740bafc8af651
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-04-12 10:28:23 +0900

    Userモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,18 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
-  has_many :followers,
-           class_name: 'Relationship',
-           foreign_key: 'follower_id',
-           dependent: :destroy,
-           inverse_of: :follower
+  
+  has_many :active_relationships, 
+            class_name: 'Relationship',
+            foreign_key: 'follower_id',
+            dependent: :destroy
+  has_many :passive_relationships, 
+            class_name: "Relationship",
+            foreign_key: "followed_id",
+            dependent: :destroy
+            
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def like(post)
     liked_posts << post unless like?(post)
@@ -22,5 +29,20 @@ class User < ApplicationRecord
 
   def like?(post)
     liked_posts.include?(post)
+  end
+  
+  def follow(other_user)
+    return if self == other_user # 自分自身はフォローできない
+    active_relationships.find_or_create_by(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    relationship = active_relationships.find_by(followed_id: other_user.id)
+    relationship&.destroy 
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
